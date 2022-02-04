b8e72f44f3f1077f64291e96b89c171fe6b8b543
John-Kimani <kimwaweru100@gmail.com>
2022-02-04 17:29:42 +0300

    adds and displays completion date in goal object aganist each item

--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -16,12 +16,12 @@ export class GoalComponent implements OnInit {
     //{id:5,  name:'Solve math homework', description:'Damn Math'},
     //{id:6,  name:'Plot my world domination plan', description:'Cause I am an evil overload'},
 
-    new Goal(1, 'Watch findin Nemo', 'Find an online version and watch merlin find his son'),
-    new Goal(2, 'Buy Cookies', 'I have to buy cookies for the parrot'),
-    new Goal(3, 'Get new Phone Case', 'Tracy has her birthday coming up soon'),
-    new Goal(4, 'Get Dog Food', 'Kiwi likes expensive snacks'),
-    new Goal(5, 'Solve Math homework', 'Damn Math'),
-    new Goal(6, 'Plot my word domination plan', 'Cause I am an evil overload'),
+    new Goal(1, 'Watch finding Nemo', 'Find an online version and watch merlin find his son', new Date(2022,0,31)),
+    new Goal(2, 'Buy Cookies', 'I have to buy cookies for the parrot', new Date(2022,1,1)),
+    new Goal(3, 'Get new Phone Case', 'Tracy has her birthday coming up soon', new Date(2022,1,2)),
+    new Goal(4, 'Get Dog Food', 'Kiwi likes expensive snacks', new Date(2022,1,3)),
+    new Goal(5, 'Solve Math homework', 'Damn Math', new Date(2022,1,2)),
+    new Goal(6, 'Plot my word domination plan', 'Cause I am an evil overload', new Date(2022,1,3)),
 
   ]
   toggleDetails(index :any){
