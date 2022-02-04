1907d6bad88c0eba8d85dc42fc43b3bebe24feeb
John-Kimani <kimwaweru100@gmail.com>
2022-02-04 17:22:25 +0300

    adds date with public access in our goal blue print

--- src/app/goal.ts
+++ src/app/goal.ts
@@ -2,8 +2,8 @@ export class Goal {
     // id: number;
     // name: string;
     // description: string;
-    showDescription: boolean;
-    constructor(public id: number, public name: string, public description: string)/*public is an access keyword*/{
+    public showDescription: boolean;//allows access across app
+    constructor(public id: number, public name: string, public description: string, public completeDate: Date)/*public is an access keyword*/{
        /*this is also an access keyword*/ this.showDescription=false; //enables description to be hidden
     }//constructior fuction with arguements to display
 }
