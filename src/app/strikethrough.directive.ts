f660fc87a761f8cd9d7bf55a06dec46011119bd7
John-Kimani <kimwaweru100@gmail.com>
2022-02-04 17:18:21 +0300

    custom directives that enable and disable strikethrough function

--- src/app/strikethrough.directive.ts
+++ src/app/strikethrough.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef } from '@angular/core'; //imports element ref
+import { Directive, ElementRef, HostListener } from '@angular/core'; //imports element ref then host listner
 
 @Directive({
   selector: '[appStrikethrough]'//attribute directive
@@ -8,6 +8,14 @@ export class StrikethroughDirective {
   constructor(private elem:ElementRef)//gives a private access to this directive
     { this.elem.nativeElement.style.textDecoration='line-through';
       }//elementref grants us acces to the DOM throught nativeelement property
+
+  @HostListener('click') onClicks(){
+    this.textDeco('line-through')
+  }
+
+  @HostListener('dblclick') onDoubleClicks(){
+    this.textDeco('None')
+  }
   private textDeco(action:string){
     this.elem.nativeElement.style.textDecoration=action;
   }//logic that creates strike through function
