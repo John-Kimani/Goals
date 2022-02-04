b5082423f5469236eca8eee4f9e703fa984504da
John-Kimani <kimwaweru100@gmail.com>
2022-02-04 16:09:35 +0300

    adds toggle button, calls its function and set loop condition

--- src/app/goal.ts
+++ src/app/goal.ts
@@ -1,5 +1,9 @@
 export class Goal {
-    id: number;
-    name: string;
-    description: string;
+    // id: number;
+    // name: string;
+    // description: string;
+    showDescription: boolean;
+    constructor(public id: number, public name: string, public description: string)/*public is an access keyword*/{
+       /*this is also an access keyword*/ this.showDescription=false; //enables description to be hidden
+    }//constructior fuction with arguements to display
 }
