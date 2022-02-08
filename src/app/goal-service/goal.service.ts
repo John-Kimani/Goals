8ed856bd191b311513340bb01836dd4819bdc207
John-Kimani <kimwaweru100@gmail.com>
2022-02-08 10:35:51 +0300

    imports goalslist into the service, creates a method which returns Goals array

--- src/app/goal-service/goal.service.ts
+++ src/app/goal-service/goal.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
+import { Goals } from '../goalList';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root'//allows usage across the app
 })
 export class GoalService {
 
+  getGoals(){
+    return Goals
+  }
   constructor() { }
 }
