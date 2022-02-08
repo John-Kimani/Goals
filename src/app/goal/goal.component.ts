ed4730fe999fa37b1987dade269a02036f711482
John-Kimani <kimwaweru100@gmail.com>
2022-02-08 10:59:12 +0300

    registers goalservice and injects it to goal component, creates new property and modifies contructor to fetch goals from the service

--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Goal } from '../goal';
+import { GoalService } from '../goal-service/goal.service';
 
 @Component({
   selector: 'app-goal',
@@ -35,7 +36,10 @@ export class GoalComponent implements OnInit {
     }
   }
 
-  constructor() { }
+  goals:Goal[]
+  constructor(goalService:GoalService) {
+    this.goals = goalService.getGoals()
+   }
 
   ngOnInit(): void {
   }
