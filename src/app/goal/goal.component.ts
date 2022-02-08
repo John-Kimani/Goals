1c9fc4ef5901849653ea2932026ebc4ca25347e3
John-Kimani <kimwaweru100@gmail.com>
2022-02-08 10:29:21 +0300

    movesgoal list from goal component to goal list file

--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -8,23 +8,6 @@ import { Goal } from '../goal';
 })
 export class GoalComponent implements OnInit {
 
-  goals:Goal[] = [
-    //{id:1,  name:'Watch finding Nemo', description:'Find an online version and watch merlin find his son'},
-    // {id:2,  name:'Buy Cookies', description:'I have to buy cookies for the parrot'},
-    //{id:3,  name:'Get new Phone Case', description:'Tracy has her birthday coming up soon'},
-    //{id:4,  name:'Get Dog Food', description:'Kiwi likes expensive snacks'},
-    //{id:5,  name:'Solve math homework', description:'Damn Math'},
-    //{id:6,  name:'Plot my world domination plan', description:'Cause I am an evil overload'},
-
-    new Goal(1, 'Watch finding Nemo', 'Find an online version and watch merlin find his son', new Date(2022,0,31)),
-    new Goal(2, 'Buy Cookies', 'I have to buy cookies for the parrot', new Date(2022,1,1)),
-    new Goal(3, 'Get new Phone Case', 'Tracy has her birthday coming up soon', new Date(2022,1,2)),
-    new Goal(4, 'Get Dog Food', 'Kiwi likes expensive snacks', new Date(2022,1,3)),
-    new Goal(5, 'Solve Math homework', 'Damn Math', new Date(2022,1,2)),
-    new Goal(6, 'Plot my word domination plan', 'Cause I am an evil overload', new Date(2022,1,3)),
-
-  ];
-  //new goals emmited from form
   addNewGoal(goal: any){
     let goalLength = this.goals.length;
     goal.id = goalLength+1;
